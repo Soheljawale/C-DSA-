a8a296ab1d9aec2a1427353c5679305af8e7e59d
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-04-05 19:23:43 +0530

    Create GFG1.cpp

---
+++ GFG1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        int sum = 0;
+        int maxSum = 0;
+
+
+// Time Complexity : o(n)
+// Space Complexity : o(1) constant 
+      
+        // First window
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        maxSum = max(maxSum, sum);
+
+        // Sliding window
+        for(int i = k; i < arr.size(); i++) {
+            sum += arr[i];        // add next element
+            sum -= arr[i - k];    // remove previous window element
+
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+};
