76002a8d7643c99d694f74a443ca2ec814523f1c
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-04-08 22:25:29 +0530

    Create 1281Leetcode.cpp

---
+++ 1281Leetcode.cpp
@@ -0,0 +1,33 @@
+// Math Problem 
+
+// 1281. Subtract the Product and Sum of Digits of an Integer
+
+// Input: n = 234  
+// Digits → 2, 3, 4  
+
+// Product = 2 × 3 × 4 = 24  
+// Sum = 2 + 3 + 4 = 9  
+
+// Output = 24 - 9 = 15
+
+
+// ⏱️ Complexity
+// Time: O(d), where d = number of digits
+// Space: O(1)
+
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        
+        int product = 1;
+        int sum = 0;
+
+        while(n){
+            int x = n % 10;
+            product *=x;
+            sum += x;
+            n /= 10;
+        }
+        return product - sum;
+    }
+};
