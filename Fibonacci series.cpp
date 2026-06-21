76eea1cfb05b6294536e26a910dab5b9c81fbe9f
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-06-21 06:52:28 +0530

    Update Fibonacci series.cpp

--- Fibonacci series.cpp
+++ Fibonacci series.cpp
@@ -1,1 +1,19 @@
 
+// Fibonacci series program using Recursion
+
+#include<iostream>
+uisng namesapce std;
+
+class Fibonacci {
+public:
+
+  int fib(int n){
+
+      //base case 
+    if(n==0 || n==1){
+        return n;
+    }
+
+  return fib(n-1) + fib(n-2);
+  }
+};
