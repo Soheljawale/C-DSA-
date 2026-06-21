1394b30fbcf06708341d21823bb8fdfdafa09bb2
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-06-21 19:09:35 +0530

    Update binarysearch.cpp

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,2 +1,30 @@
 // binary search  with recursion
 
+class Solution {
+public:
+//helper function
+
+int binarysearch(vector<int>& arr, int target, int st, int end){
+
+
+    if(st<=end){
+        
+        int mid = st + end/2;
+
+        if(arr[mid]== target) return mid;
+
+        else if(arr[mid] <= target){
+            return binarysearch(arr, target, mid+1, end);
+        } else {
+            return binarysearch(arr, target, st, mid-1);
+        }
+
+    }
+
+    return -1;
+}
+    int search(vector<int>& arr, int target) {
+        
+        return binarysearch(arr, target, 0, arr.size()-1);
+    }
+};
