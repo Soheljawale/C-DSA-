094486fed906c1b1bda5958d2951506ebbb1b984
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-06-26 11:56:36 +0530

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,49 @@
 
+#include<iostream>
+using namespace std;
+
+
+int queue[5];
+int front = 0;
+int rear = -1;
+
+int main(){
+    
+    //insertion at rear in queue
+    queue[++rear] = 10;
+    queue[++rear] = 20;
+    queue[++rear] = 30;
+    queue[++rear] = 40;
+    queue[++rear] = 50;
+    
+    //displaying queue after insertion
+    for(int i = front; i <= rear; i++){
+        cout<<queue[i]<<" ";
+    }
+    cout<<endl;
+    
+    //dequeue operation on queue
+    while(front<2){
+        cout<<"dequeue operation is perform"<<queue[front++]<<endl;
+    }
+    
+    //displayed queue after pop operation
+    
+    for(int i = front; i <= rear; i++){
+        
+        cout<<queue[i]<<" "<<endl;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
