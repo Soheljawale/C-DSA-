ad6299ea536dc23887d36670f6c173c5b7673ed7
Soheljawale <141735243+Soheljawale@users.noreply.github.com>
2026-07-09 22:46:37 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+    
+def test_add():
+    a = 20
+    b =30
+    result = add(a,b)
+    return result
+    
+if __name__ == "__main__":
+    print("result", test_add())
+    
+    
+    
